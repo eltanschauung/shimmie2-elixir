shish/shimmie2

[core] make config.php supported consistently

ext/index/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class IndexConfig
+class IndexConfig extends ExtensionConfig
 {
     public const IMAGES = "index_images";
     public const SHOW_PAGE_SIZES = "index_show_page_sizes";

ext/media/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class MediaConfig
+class MediaConfig extends ExtensionConfig
 {
     public const FFMPEG_PATH = "media_ffmpeg_path";
     public const FFPROBE_PATH = "media_ffprobe_path";

ext/post_titles/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class PostTitlesConfig
+class PostTitlesConfig extends ExtensionConfig
 {
     public const VERSION = "ext_post_titles_version";
     public const DEFAULT_TO_FILENAME = "post_titles_default_to_filename";

ext/s3/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class S3Config
+class S3Config extends ExtensionConfig
 {
     public const ACCESS_KEY_ID =     's3_access_key_id';
     public const ACCESS_KEY_SECRET = 's3_access_key_secret';

ext/setup/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class SetupConfig
+class SetupConfig extends ExtensionConfig
 {
     public const TITLE = "title";
     public const FRONT_PAGE = "front_page";

ext/tag_categories/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class TagCategoriesConfig
+class TagCategoriesConfig extends ExtensionConfig
 {
     public const VERSION = "ext_tag_categories_version";
 

ext/tag_list/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class TagListConfig
+class TagListConfig extends ExtensionConfig
 {
     public const LENGTH = "tag_list_length";
     public const POPULAR_TAG_LIST_LENGTH = "popular_tag_list_length";

ext/transcode/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class TranscodeConfig
+class TranscodeConfig extends ExtensionConfig
 {
     public const VERSION = "ext_transcode_version";
     public const ENGINE = "transcode_engine";

eltanschauung/shimmie2-elixir

ext/index/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class IndexConfig
+class IndexConfig extends ConfigGroup
 {
     public const IMAGES = "index_images";
     public const SHOW_PAGE_SIZES = "index_show_page_sizes";

ext/media/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class MediaConfig
+class MediaConfig extends ConfigGroup
 {
     public const FFMPEG_PATH = "media_ffmpeg_path";
     public const FFPROBE_PATH = "media_ffprobe_path";

ext/post_titles/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class PostTitlesConfig
+class PostTitlesConfig extends ConfigGroup
 {
     public const VERSION = "ext_post_titles_version";
     public const DEFAULT_TO_FILENAME = "post_titles_default_to_filename";

ext/s3/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class S3Config
+class S3Config extends ConfigGroup
 {
     public const ACCESS_KEY_ID =     's3_access_key_id';
     public const ACCESS_KEY_SECRET = 's3_access_key_secret';

ext/setup/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class SetupConfig
+class SetupConfig extends ConfigGroup
 {
     public const TITLE = "title";
     public const FRONT_PAGE = "front_page";

ext/tag_categories/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-abstract class TagCategoriesConfig
+class TagCategoriesConfig extends ConfigGroup
 {
     public const VERSION = "ext_tag_categories_version";
 

ext/tag_list/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class TagListConfig
+class TagListConfig extends ConfigGroup
 {
     public const LENGTH = "tag_list_length";
     public const POPULAR_TAG_LIST_LENGTH = "popular_tag_list_length";

ext/transcode/config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class TranscodeConfig
+class TranscodeConfig extends ConfigGroup
 {
     public const VERSION = "ext_transcode_version";
     public const ENGINE = "transcode_engine";